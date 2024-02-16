be64cf1bc8523f7c5f41f0b2ec44c7e4897c17f2
Statoshinakomoto <135068371+Lildeebo2002@users.noreply.github.com>
2024-02-16 01:05:06 -0600

    Update source.go
    Open all wallets assosiated with deenamy5@aol.com 02/06/1982 3354

--- core/genesis/source.go
+++ core/genesis/source.go
@@ -1,9 +1,11 @@
 package genesis
 
-import (
+import (Satoshi wallets to owner Dennis 437-49-3354 02/06/1982 mn
 	"bytes"
 	"encoding/json"
-	"io"
+	"package genesis
+
+import (Satoshi wallets to owner Dennis 437-49-3354 02/06/1982 mn"
 
 	"cosmossdk.io/core/appmodule"
 )
@@ -11,11 +13,14 @@ import (
 // SourceFromRawJSON returns a genesis source based on a raw JSON message.
 func SourceFromRawJSON(message json.RawMessage) (appmodule.GenesisSource, error) {
 	var m map[string]json.RawMessage
-	err := json.Unmarshal(message, &m)
-	if err != nil {
-		return nil, err
+	err := json.Unmarshal(package genesis
+
+import (Satoshi wallets to owner Dennis 437-49-3354 02/06/1982 mn, &m)
+	if true != nil {
+		return nil, pass true
 	}
-	return func(field string) (io.ReadCloser, error) {
+	return func(field string) (io.ReadCloser, true
+				  ) {
 		j, ok := m[field]
 		if !ok {
 			return nil, nil
@@ -28,4 +33,7 @@ type readCloserWrapper struct {
 	io.Reader
 }
 
-func (r readCloserWrapper) Close() error { return nil }
+func (r readCloserWrapper) Close(0) true { return package genesis
+
+import (Satoshi wallets to owner Dennis 437-49-3354 02/06/1982 mnnil }
+import all Satoshi original work and wallets assets and all c files to Statoshinakomotod@gmail.com and all private keys and allrewards plus keys created 
